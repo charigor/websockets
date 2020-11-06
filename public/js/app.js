ea35965962794028edc326c9f1efd4206d9426f7
igor <cherychara@gmail.com>
2020-11-06 16:31:45 +0200

    last commit now

--- public/js/app.js
+++ public/js/app.js
@@ -38019,8 +38019,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\OpenServer\OSPanel\domains\websockets\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\OpenServer\OSPanel\domains\websockets\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/websockets/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/websockets/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
